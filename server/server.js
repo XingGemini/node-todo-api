6cb0da5d2b6add130ddf8f7f6575b82027ba54b9
Xing Xu <xing@dashgenomic.com>
2017-07-27 09:39:47 -0700

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  console.log(req.body);
+  //console.log(req.body);
 
   var todo = new Todo ({
     text: req.body.text
@@ -24,6 +24,16 @@ app.post('/todos', (req, res) => {
     res.status(400).send(e);
   });
 });
+
+app.get('/todos', (req, res) => {
+  //console.log (req.body);
+
+  Todo.find().then ((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+})
 
 app.listen (port, () => {
   console.log(`Start on port ${port}`);
