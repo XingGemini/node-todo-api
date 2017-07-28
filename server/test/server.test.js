88f946cae0cfc1910cf99e639639cad445adc2cf
Xing Xu <xing@dashgenomic.com>
2017-07-27 21:05:12 -0700

    add tests for GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,8 +4,16 @@ const request = require ('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todos = [
+    { text : "Todo 1"},
+    { text : "Todo 2"},
+    { text : "Todo 3"}
+];
+
 beforeEach ((done) => {
-  Todo.remove({}).then (() => done());
+  Todo.remove({}).then (() => {
+    return Todo.insertMany(todos);
+  }).then (() => done());
 });
 
 describe ('POST /todos', () => {
@@ -25,8 +33,8 @@ describe ('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(1);
-          expect(todos[0].text).toBe(text);
+          expect(todos.length).toBe(4);
+          expect(todos[3].text).toBe(text);
           done()
         }).catch ((e) => done(e));
       });
@@ -48,10 +56,22 @@ describe ('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(3);
           //expect(todos[0].text).toBe(text);
           done()
         }).catch ((e) => done(e));
       });
+  });
+});
+
+describe ('GET /todos', () => {
+  it ('should get all today', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(3);
+      })
+      .end(done);
   });
 });
