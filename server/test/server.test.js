00b879ad407fc495453522964981828145c6cdba
Xing Xu <xing@dashgenomic.com>
2017-07-28 10:22:58 -0700

    Test DELETE /todos/:id route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -111,3 +111,43 @@ describe ('GET /todos:id', () => {
       .end(done);
   });
 });
+
+
+describe ('DELETE /todos:id', () => {
+  it ('should DELETE todo by id', (done) => {
+    var hexId = todos[0]._id.toHexString()
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done (err);
+        }
+
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch ((e) => done(e));
+
+      });
+  });
+
+  it ('should return 404 if todo not found during DELETE', (done) => {
+    var id = new ObjectID();
+    request(app)
+      .get(`/todos/${id.toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it ('should return 404 if non-object during DELETE', (done) => {
+    var id = new ObjectID();
+    request(app)
+      .get(`/todos/123`)
+      .expect(404)
+      .end(done);
+  });
+});
