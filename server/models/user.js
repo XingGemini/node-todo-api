24d2395eb9f0cdea69f86897d1f42234da020f2e
Xing Xu <xing@dashgenomic.com>
2017-07-31 15:54:20 -0700

    Add POST /user/login route

--- server/models/user.js
+++ server/models/user.js
@@ -79,6 +79,30 @@ UserSchema.statics.findByToken = function (token) {
   });
 };
 
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      // bcrypt.compare(password, hashedPassword, (err, res) => {
+      //   console.log(res);
+      // });
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+
+    });
+  })
+};
+
 UserSchema.pre('save', function(next) {
   var user = this;
 
