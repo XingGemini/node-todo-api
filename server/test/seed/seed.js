c8811067fcc06b0df76a6a05224f51e58d96e568
Xing Xu <xing@dashgenomic.com>
2017-07-31 19:59:57 -0700

    add test cases for POST /users/login

--- server/test/seed/seed.js
+++ server/test/seed/seed.js
@@ -40,10 +40,10 @@ const users = [
       _id : userTwoId,
       email : 'xing2@gmail.com',
       password: 'userTwoPass',
-      tokens: [{
-        access:'auth',
-        token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString()
-      }]
+      // tokens: [{
+      //   access:'auth',
+      //   token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
+      // }]
     }
 ];
 
